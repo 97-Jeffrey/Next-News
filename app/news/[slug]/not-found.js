ff8f5f1d078f136431fd09ddf348e5c260fde0ba
Yu Hao Shao <yuhaoshao@Yus-MacBook-Air.local>
2025-01-16 16:12:45 -0800

    added new project

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFoundPage(){
+    return <div id='error'>
+       <h1>Not Found !</h1>
+       <p>Sorry, we cannot find the requested article.</p>
+    </div>
+   }
---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFoundPage(){
+ return <div id='error'>
+    <h1>Not Found !</h1>
+    <p>The requested resource can't be found</p>
+ </div>
+}
